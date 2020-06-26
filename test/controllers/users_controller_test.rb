9f561f22063d406b12d783e7d05d55221a235f12
Hector Beita <hgarcia@cecropiasolutions.com>
2020-06-26 09:29:55 -0600

    Remove duplicate test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  def test_users_new
-    get users_new_url
-    assert_response :success
-  end
-
   def test_signup
     get signup_path
     assert_response :success
