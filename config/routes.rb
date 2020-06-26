ac20983a246948f57041106e156559a2eee5c7c0
Hector Beita <hgarcia@cecropiasolutions.com>
2020-06-25 19:05:36 -0600

    Rubocop style changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get '/home', to: 'static_pages#home'
-  root 'static_pages#home'
+  get "/home", to: "static_pages#home"
+  root "static_pages#home"
 end
