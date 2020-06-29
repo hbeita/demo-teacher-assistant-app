111bacf1e129da481f79bc0bf134803e002e05ce
Hector Beita <hgarcia@cecropiasolutions.com>
2020-06-28 20:31:19 -0600

    Add init devise for user-demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get "/home", to: "static_pages#home"
   get "/signup", to: "users#new"
   root "static_pages#home"
